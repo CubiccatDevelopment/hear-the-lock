879585cc2b0ceb6a8ca14af62c1df855f8214f79
jkicmal <jakub.kicmal@gmail.com>
2019-05-08 11:12:30 +0200

    initial commit

---
+++ Assets/Scripts/MenuScripts/ButtonsFunc.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonsFunc : MonoBehaviour {
+
+
+	public void LoadLevelMenu(){
+		SceneManager.LoadScene (1);
+	}
+	public void LoadMenu(){
+		SceneManager.LoadScene (0);
+	}
+}
